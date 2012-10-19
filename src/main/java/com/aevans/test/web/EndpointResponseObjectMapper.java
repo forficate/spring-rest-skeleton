cc3fbc540d200db4cd211d46600008cc290d2767
Adam Evans <adam@agileand.me>
2012-10-20 00:55:46 +0100

    Renemaed file to match others.

---
+++ src/main/java/com/aevans/test/web/EndpointResponseObjectMapper.java
@@ -0,0 +1,18 @@
+package com.aevans.test.web;
+
+import org.codehaus.jackson.Version;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig.Feature;
+import org.codehaus.jackson.map.module.SimpleModule;
+
+public class EndpointResponseObjectMapper extends ObjectMapper {
+
+	public EndpointResponseObjectMapper() {
+		SimpleModule simpleModule = new SimpleModule("SimpleModule", 
+	            new Version(1,0,0,null));
+		
+		simpleModule.addSerializer(EndpointResponse.class, new EndpointResponseSerializer());
+		registerModule(simpleModule);
+		configure(Feature.INDENT_OUTPUT, true);
+	}
+}
